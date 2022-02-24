c02ba9641fd81590322755aa72989f311a12e929
NIRAJ MAHAJAN <96792295+Nirajmahajan27@users.noreply.github.com>
2022-02-24 11:14:15 +0530

    Add files via upload

--- diceoutcomes.c
+++ diceoutcomes.c
@@ -18,27 +18,53 @@ printf("\n\n\n                   ************************************** ROLLING 
 switch (thrown)
 {
     case 1 :
-      printf("\n\n\n\n                                                            It's ONE.");
+      printf("\n                                                              ...........           \n");
+      printf("                                                              :         :           \n");
+      printf("                                                              :    *    :           \n");
+      printf("                                                              :         :           \n");
+      printf("                                                              :.........:           \n");
+
+
       break;
 
     case 2 :
-      printf("\n\n\n\n                                                            It's TWO.");
+      printf("\n                                                              ...........           \n");
+      printf("                                                              : *       :           \n");
+      printf("                                                              :         :           \n");
+      printf("                                                              :       * :           \n");
+      printf("                                                              :.........:           \n");
       break;
 
     case 3 :
-      printf("\n\n\n\n                                                            It's THREE.");
+      printf("\n                                                              ...........           \n");
+      printf("                                                              : *       :           \n");
+      printf("                                                              :    *    :           \n");
+      printf("                                                              :       * :           \n");
+      printf("                                                              :.........:           \n");
       break;
 
     case 4 :
-      printf("\n\n\n\n                                                            It's FOUR.");
+      printf("\n                                                              ...........           \n");
+      printf("                                                              : *     * :           \n");
+      printf("                                                              :         :           \n");
+      printf("                                                              : *     * :           \n");
+      printf("                                                              :.........:           \n");
       break;
 
     case 5 :
-      printf("\n\n\n\n                                                            It's FIVE.");
+      printf("\n                                                              ...........           \n");
+      printf("                                                              : *     * :           \n");
+      printf("                                                              :    *    :           \n");
+      printf("                                                              : *     * :           \n");
+      printf("                                                              :.........:           \n");;
       break;
 
     case 6 :
-      printf("\n\n\n\n                                                            It's SIX.");
+      printf("\n                                                              ...........           \n");
+      printf("                                                              : *     * :           \n");
+      printf("                                                              : *     * :           \n");
+      printf("                                                              : *     * :           \n");
+      printf("                                                              :.........:           \n");
       break;
     
 }
